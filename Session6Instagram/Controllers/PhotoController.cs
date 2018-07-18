308cc76bcebbefc5beb62b40b019e77789e461b3
John Lange <lange.john.m@gmail.com>
2018-07-18 19:41:15 -0400

    Initial commit

---
+++ Session6Instagram/Controllers/PhotoController.cs
@@ -0,0 +1,40 @@
+﻿using Session6Instagram.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session6Instagram.Controllers
+{
+    public class PhotoController : Controller
+    {
+        public ActionResult UploadPhoto()
+        {
+            InstagramDbContext database = new InstagramDbContext();
+            var users = database.Users.ToList();
+            return View(users);
+        }
+
+        [HttpPost]
+        public string SavePhoto(string photo, int userId, string caption)
+        {
+            InstagramDbContext database = new InstagramDbContext();
+            Photo temp_photo = new Photo();
+            temp_photo.Picture = photo;
+            temp_photo.Caption = caption;
+            temp_photo.PhotoUser = database.Users.Find(userId);
+            temp_photo.Date = DateTime.Now;
+            database.Photos.Add(temp_photo);
+            database.SaveChanges();
+
+            return "I worked?";
+        }
+
+        public ActionResult Feed()
+        {
+            var database = new InstagramDbContext();
+            return View(database.Photos.ToList());
+        }
+    }
+}
